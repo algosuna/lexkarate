5f9672763baf67f178b639516aba45d190f56805
Andrea <info@andyosuna.com>
2013-03-12 12:41:20 -0700

    Layout
    Doing the HTML and adding a few wp.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+<head>
+	<meta charset="<?php bloginfo('charset');?>" />
+	<meta name="viewport" content="width=device-width" />
+	<meta name="description" content="<?php bloginfo('description');?>" />
+
+	<title><?php bloginfo('name');?> - <?php bloginfo('description');?></title>
+
+	<link rel="stylesheet" href="http://andyosuna.com/css/reset.css" media="all" />
+	
+	<?php wp_head();?>
+	
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>" media="screen" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<!--[if IE]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+</body>
+</html>
