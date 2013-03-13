d0d1863177e156f43b903eb9de9b73ee682df4ed
Andrea <info@andyosuna.com>
2013-03-13 12:51:42 -0700

    Layout - Temp
    Used a standard template to work on the CSS and other layout. Using
    responsive bootstrap.

--- index.php
+++ index.php
@@ -1,23 +1,128 @@
 <!DOCTYPE html>
-<html <?php language_attributes();?>>
+<html lang="es">
 <head>
-	<meta charset="<?php bloginfo('charset');?>" />
-	<meta name="viewport" content="width=device-width" />
-	<meta name="description" content="<?php bloginfo('description');?>" />
+	<script type="text/javascript" src="//use.typekit.net/wxm5gvs.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
-	<title><?php bloginfo('name');?> - <?php bloginfo('description');?></title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="Karate Japon&eacute;s del estilo Shotokan. Tijuana, B.C. Afiliados a Funakoshi Shotokan Karate Association (FSKA)" />
 
-	<link rel="stylesheet" href="http://andyosuna.com/css/reset.css" media="all" />
-	
-	<?php wp_head();?>
-	
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>" media="screen" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<title>LexKarate.com | Lex Shotokan Karate Do | Karate Tijuana | M&eacute;xico</title>
+
+	<link rel="stylesheet" type="text/css" href="http://andyosuna.com/css/bootstrap.min.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="http://andyosuna.com/css/bootsrap-responsive.min.css" />
+
+	<link rel="stylesheet" type="text/css" href="wp-content/themes/lexkarate/style.css" media="screen" />
+
 	<!--[if IE]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 </head>
+ 
+<body class="home">
+	<header>
+		<div class="container">
+			<h1><a href="/">Lex Karate</a></h1>
+			<nav>
+				<ul class="tj">
+					<li><a href="">Inicio</a></li>
+					<li><a href="">Horario</a></li>
+					<li><a href="">Instructores</a></li>
+					<li><a href="">Afiliaciones</a></li>
+					<li><a href="">Contacto</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
 
-<body>
+	<section id="content">
+
+		<article id="post-1" class="post">
+			<h2 class="entry-title"> <a href="/link to single post/"> Post Title </a></h2>
+			<div class="postmeta">
+				<span class="author"> Posted by: Bob </span>
+				<span class="date"> December 27, 2011 </span>
+				<span class="num-comments"> one comment </span>
+				<span class="categories"> <a href="/category/updates/" title="View all posts in Updates" rel="category tag">Updates</a> </span>
+				<span class="tags">tag, tag2, tag3</span>
+			</div><!-- end postmeta -->
+			<div class="entry-content">
+				<p>A short portion of the post content. Etiam posuere adipiscing ante a euismod. Aliquam rutrum tempus ipsum a pulvinar. Cras arcu tellus, aliquam ac rutrum sit amet, consectetur et sapien. [...]</p>
+			</div>
+		</article>
+
+		<article id="post-2" class="post">
+			<h2 class="entry-title"> <a href="/link to single post/"> Post Title </a></h2>
+			<div class="postmeta">
+				<span class="author"> Posted by: Bob </span>
+				<span class="date"> December 27, 2011 </span>
+				<span class="num-comments"> one comment </span>
+				<span class="categories"> <a href="/category/updates/" title="View all posts in Updates" rel="category tag">Updates</a> </span>
+				<span class="tags">tag, tag2, tag3</span>
+			</div><!-- end postmeta -->
+			<div class="entry-content">
+				<p>A short portion of the post content. Etiam posuere adipiscing ante a euismod. Aliquam rutrum tempus ipsum a pulvinar. Cras arcu tellus, aliquam ac rutrum sit amet, consectetur et sapien. [...]</p>
+			</div>
+		</article>
+
+		<article id="post-3" class="post">
+			<h2 class="entry-title"> <a href="/link to single post/"> Post Title </a></h2>
+			<div class="postmeta">
+				<span class="author"> Posted by: Bob </span>
+				<span class="date"> December 27, 2011 </span>
+				<span class="num-comments"> one comment </span>
+				<span class="categories"> <a href="/category/updates/" title="View all posts in Updates" rel="category tag">Updates</a> </span>
+				<span class="tags">tag, tag2, tag3</span>
+			</div><!-- end postmeta -->
+			<div class="entry-content">
+				<p>A short portion of the post content. Etiam posuere adipiscing ante a euismod. Aliquam rutrum tempus ipsum a pulvinar. Cras arcu tellus, aliquam ac rutrum sit amet, consectetur et sapien. [...]</p>
+			</div>
+		</article>
+
+	</section><!-- end #content -->
+
+	<aside id="sidebar">
+		<section id="categories" class="widget">
+			<h3 class="widget-title"> Categories </h3>
+			<ul>
+				<li><a href="/category/updates/">Updates</a> (21)</li>
+				<li><a href="/category/sandwiches/">Sandwiches</a> (21)</li>
+				<li><a href="/category/misc/">Miscellaneous</a> (21)</li>
+			</ul>
+		</section>
+		<section id="archives" class="widget">
+			<h3 class="widget-title"> Archives </h3>
+			<ul>
+				<li><a href="/2011/" title="2011">2011</a></li>
+				<li><a href="/2008/" title="2008">2008</a></li>
+				<li><a href="/2007/" title="2007">2007</a></li>
+			</ul>
+		</section>
+		<section id="tags" class="widget">
+			<h3 class="widget-title"> Tags </h3>
+			<ul>
+				<li>tag1</li>
+				<li>another tag</li>
+				<li>taggity tag</li>
+			</ul>
+		</section>
+		<section id="meta" class="widget">
+			<h3 class="widget-title"> Meta </h3>
+			<ul>
+				<li><a href="/wp-admin/">Site Admin</a></li>
+				<li><a href="/wp-login.php?action=logout&#038;_wpnonce=394ec759dd">Log out</a></li>
+			</ul>
+		</section>
+	</aside><!-- end #sidebar -->
+
+	<footer>
+		&copy; 2012 by Author. All Rights Reserved.
+	</footer>
+
+
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
 </body>
 </html>
